7a15fe0a5cb944bc0cf0a933c20164b763908b18
Muhammad Kamran <MuhammadKamran.dev@gmail.com>
2024-05-20 12:58:09 -0700

    Include subview Practice added

--- practice/resources/views/about.blade.php
+++ practice/resources/views/about.blade.php
@@ -1,44 +1,7 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>About Page!</title>
-</head>
+@include('pages.navbar', ['title' => 'About Page!'])
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class=" nav-item">
-                        <a class="nav-link " href="{{ route('gallery') }}">Gallery</a>
-                    </li>
-                    <li class=" nav-item">
-                        <a class="nav-link active" href="{{ route('about') }}">About Us</a>
-                    </li>
 
-                </ul>
-
-            </div>
-        </div>
-    </nav>
     <div class="container my-5">
 
         <form>
@@ -60,13 +23,5 @@
         </form>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
 
 </body>
-
-</html>
